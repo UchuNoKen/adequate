e8e88b679073d53c9278112b1822115dc5ad1f55
UchuNoKen <xen.o.fite@gmail.com>
2019-04-02 13:18:01 -0400

    functional flock refactor

--- ch-001/001-code.js
+++ ch-001/001-code.js
@@ -24,3 +24,34 @@ Flock.prototype.breed = function(other) {
 let flock_a = new Flock(4);
 let flock_b = new Flock(2);
 let flock_c = new Flock(0);
+
+let result = flock_a
+  .conjoin(flock_c)
+  .breed(flock_b)
+  .conjoin(flock_a.breed(flock_b)).seagulls;
+
+// ! This is bad
+// It makes the point that state and mutable values are hard to follow,
+//   even in small examples
+
+// --> A More Functional Approach
+
+let conjoin = function(flock_x, flock_y) {
+  return flock_x + flock_y;
+};
+let breed = function(flock_x, flock_y) {
+  return flock_x * flock_y;
+};
+
+let flock_a = 4;
+let flock_b = 2;
+let flock_c = 0;
+
+let result = conjoin(breed(flock_b, conjoin(flock_a, flock_c)), breed(flock_a, flock_b));
+
+// => 16
+
+// In the two custom functions, we're just working with addition (conjoin), and
+//    multiplication (breed).
+
+// --> Rename custom functions to 'multiply' and 'add'
