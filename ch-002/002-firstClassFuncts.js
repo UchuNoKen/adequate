bdcc2ea0585f3b82e8715b205c912aba63ed8b57
UchuNoKen <xen.o.fite@gmail.com>
2019-04-02 14:00:16 -0400

    ch-2 initial commit

---
+++ ch-002/002-firstClassFuncts.js
@@ -0,0 +1,15 @@
+/**
+ * Ch.2 First-Class Functions
+ *
+ */
+
+// A quick review ------------------------------------------------------------
+
+// JS functions can be passed as data, returned from functions, and assigned to variables
+// This makes them first class
+
+// Ex.
+
+hi = name => {
+  return `Hi ${name}`;
+};
