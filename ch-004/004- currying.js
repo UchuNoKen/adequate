efd528ccf6e639951c7a4652c0b4912ce55c91d6
UchuNoKen <xen.o.fite@gmail.com>
2019-04-02 16:51:59 -0400

    currying intro examples

--- ch-004/004- currying.js
+++ ch-004/004- currying.js
@@ -2,3 +2,37 @@
  * Ch.4 Currying
  *
  */
+
+// currying:
+//   - the concept is that you can call a function with less arguments
+//     than it expects
+//   - it returns a function that takes the remaining arguments
+
+// Ex.
+// function expression
+let add = function(x) {
+  return function(y) {
+    return x + y;
+  };
+};
+
+// arrow functions
+let add = x => {
+  return y => {
+    return x + y;
+  };
+};
+
+// concise arrow functions
+let add = x => y => x + y;
+
+let increment = add(1);
+let addTen = add(10);
+
+increment(4); // 5
+addTen(5); // 15
+
+// add takes one argument, returns a function
+// the retured function has the first argument via closure
+
+// curried functions
